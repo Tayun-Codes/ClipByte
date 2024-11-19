cfa4e3c58cdaf5e4ec0e47384ab890a5571beff2
TayunCodes <tayunparkdev@gmail.com>
2024-11-18 23:56:55 -0500

    init

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://tayunparkdev:Ha3NfWMqHBiS31fl@aws-test-cluster.tby5r.mongodb.net/demo?retryWrites=true&w=majority&appName=AWS-Test-Cluster', 
+    'dbName': 'demo'
+};
